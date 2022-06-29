c16ab252bdf34561351d89fad777fc770ee5c238
Chandan2485 <chandan96kar@gmail.com>
2022-06-29 18:32:34 +0530

    test_1

--- weekend_test/test_1.js
+++ weekend_test/test_1.js
@@ -72,5 +72,105 @@ function amPm(hr,min){
     console.log(amPm(1,1))
 
 
+    // Q.3  Write a code to take time in seconds and convert it into hour, mins and seonds
 
-// Q.3  
+
+    function time(seconds){
+
+      // return ?
+    let h=Math.floor(seconds/3600);
+      // console.log(h)
+    let m=Math.floor((seconds%3600)/60);
+       // console.log(m)
+    let s=(seconds%3600)%60
+       // console.log(s)
+      return ('In '+seconds+' seconds '+h+' hours , '+m+' minutes and '+s+' seconds are possible')
+    }
+    console.log(time(5000))
+
+
+
+// Q 4.  Write a code to take temperature in Fahrenheit and convert it to Celsius.
+
+
+
+function temperature(temp){
+  return (temp-32)*(5/9)
+}
+
+console.log(temperature(57))
+
+
+// Q 5.   Take a number and find the "ans" by sum of sqaure of digits. 
+// once you find this "ans" repeat the steps 
+// ( I mean find sum of square of digits of ans) and repeat this process until you get 1.
+
+
+function checkNumber(n){
+  // let sum=0
+  let counter =0;
+  while(counter < 100 && n !=1 ){
+    let sum=0
+        while(n!=0){
+          let x=n%10
+          sum=sum+(x*x)
+          n=Math.floor(n/10)
+           // console.log(n)
+        }
+    counter++;
+    n=sum
+    // console.log(n)
+  }
+  
+  if(n==1){
+    return ('CodeSpecial')
+  }
+    else{
+       return ('SomethingUnusual')
+    }
+  }
+  console.log(checkNumber(173))
+
+
+
+// Q 6.   Radhika wants to calculate the no. of balls present in n boxes. The nth box has n balls inside it,
+//  n-1 box has n-1 balls inside it and so on till 1st box. Can you help her to calculate the total no. of balls
+//   present in n boxes.
+
+
+function totalBalls(n){
+  let sum=0
+  for(let i=1;i<=n;i++){
+    sum=sum+i
+  }
+  return sum
+  
+    
+  }
+  console.log(totalBalls(5))
+
+
+
+  // Q7.  Take two numbers and find product of all numbers between them that satisfy following condition 
+//   (inclusive range):
+// -> Numbers should be even
+// -> Second last digit of number is 4
+
+
+
+function product(a,b){
+  let p=1;
+ for(let i=a; i<=b;i++){
+   let c=Math.floor(i/10);
+   if(i%2==0 && c%10==4){
+     p=p*i
+   }
+ }
+ if(p>1){
+   return p
+ }
+ else {
+   return 0
+ }
+}
+console.log(product(40,41))
