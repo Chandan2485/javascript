ad60f2733e03069524a492856de19e31f0dc68a6
Chandan2485 <chandan96kar@gmail.com>
2022-07-01 13:51:18 +0530

    Q5

--- Pattren/pattren.js
+++ Pattren/pattren.js
@@ -1,3 +1,46 @@
+// Question 1.
+
+
+// let n=11;
+// for(i=1;i<=n;i++){
+//  let str=''
+//  for(j=1;j<=n;j++){
+//     if(i==j || (i+j==n+1)){
+//         str=str+`${' * '}`
+//     }
+//     else{
+//         str=str+`${'   '}`
+//     }
+//  }
+//  console.log(str)
+// }
+
+
+
+// Question 2.
+
+
+// let n=5;
+// for(i=n;i>=1;i--){
+//  let str=''
+//  for(j=n;j>i;j--){
+//    str=str+`${' '}`
+//  }
+//  for(k=1;k<(i*2);k++){
+//  if(k>1 && k<(i*2)-1){
+//  str=str+`${' '}`
+//  }
+// else{
+//   str=str+`${'*'}`
+// }
+
+//  }
+//  console.log(str)
+// }
+
+
+
+
 
 // Question 3.
 
@@ -48,41 +91,48 @@
 
 
 
+// Question 5.
 
 
+// let x=9;
+// let y=(x+1)/2
 
-
-// Question 1.
-
-
-// let n=11;
-// for(i=1;i<=n;i++){
-//  let str=''
-//  for(j=1;j<=n;j++){
-//     if(i==j || (i+j==n+1)){
-//         str=str+`${' * '}`
-//     }
-//     else{
-//         str=str+`${'   '}`
-//     }
-//  }
-//  console.log(str)
+//                                 //  for upper part
+// for(i=1;i<=y;i++){
+//   let a=i
+//   // let b=i
+//   let str=""
+//   for(j=i;j<y;j++){
+//     str=str+`${'  '}`
+//   }
+//   for(k=1;k<=i;k++){
+//     str=str+`${a} `
+//     a++
+//   }
+//   a--
+//   for(l=1;l<i;l++){
+//     a--
+//     str=str+`${a} `
+//   }
+//   console.log(str)
 // }
 
+//                                   // for lower part 
 
-
-// Question 2.
-
-
-let n=3;
-for(i=1;i<=n;i++){
- let str=''
- for(j=n;j>i;j--){
-   str=str+`${'   '}`
- }
- for(k=1;k<(i*2);k++){
-  str=str+" * "
- }
- console.log(str)
-}
-
+// for(i=1;i<=y-1;i++){
+//   str=""
+//   for(j=1;j<=i;j++){
+//     str=str+`${'  '}`
+//   }
+//   let z=y-i
+//   for(k=i;k<=y-1;k++){
+//     str=str+`${z} `
+//     z++
+//   }
+//   z--
+//   for(l=i;l<y-1;l++){
+//     z--
+//     str=str+`${z } `
+//   }
+//   console.log(str)
+// }
