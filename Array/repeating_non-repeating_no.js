a35a5824e48b87d925d4c7b611f2f5ca13823bd5
Chandan2485 <chandan96kar@gmail.com>
2022-07-07 17:02:16 +0530

    add

---
+++ Array/repeating_non-repeating_no.js
@@ -0,0 +1,99 @@
+// ## 1st repeating no from starting
+
+// let nums=[1,2,7,3,4,4,5,6,7,10,3,4,5,6,7,8]
+
+// function a(){
+// for(i=0;i<nums.length;i++){
+//     let c=0
+//   for(j=0;j<nums.length;j++){
+//     if(nums[i]==nums[j]){
+//         c++
+//         if(c>1){
+//         return nums[i]
+//      }
+//     }
+//   }
+// }
+// }
+// console.log(a())
+
+
+
+// ## 1st non-repeating no from starting
+
+// let nums=[1,1,2,7,3,4,4,5,6,7,10,3,4,5,6,7,8,2,10]
+
+// function a(){
+//     for(i=0;i<nums.length;i++){
+//         let c=0
+//       for(j=0;j<nums.length;j++){
+//         if(nums[i]==nums[j]){
+//             c++
+//         }
+//     }
+//     if(c==1){
+//         return nums[i]
+//       }
+//     }
+// }
+// console.log(a())
+
+
+// ## 1st repeating no from endinging
+
+// let nums=[1,2,3,4,4,5,6,10,3,4,5,6,7,8]
+
+// function a(){
+// for(i=(nums.length-1);i>0;i--){
+//     let c=0
+//   for(j=0;j<nums.length;j++){
+//     if(nums[i]==nums[j]){
+//         c++
+//         if(c>1){
+//         return nums[i]
+//      }
+//     }
+//   }
+// }
+// }
+// console.log(a())
+
+
+// ## 1st non-repeating no from ending
+
+// let nums=[1,2,3,4,4,5,6,10,3,4,5,6,7,8,8]
+
+// function a(){
+// for(i=(nums.length-1);i>0;i--){
+//     let c=0
+//   for(j=0;j<nums.length;j++){
+//     if(nums[i]==nums[j]){
+//         c++
+//     }
+//   }
+//   if(c==1){
+//   return nums[i]
+//  }
+// }
+// }
+// console.log(a())
+
+
+// ## 1st  n-repeating (3-times) no from starting
+
+// let nums=[1,2,7,3,4,4,5,6,7,10,3,3,4,5,6,7,8,1,1,1]
+
+// function a(){
+// for(i=0;i<nums.length;i++){
+//     let c=0
+//   for(j=0;j<nums.length;j++){
+//     if(nums[i]==nums[j]){
+//         c++
+//      }
+//   }
+//   if(c==3){
+//             return nums[i]
+//          }
+// }
+// }
+// console.log(a())
