e9d85501ae3f30086284ebd4ef90b607f01ede33
Chandan2485 <chandan96kar@gmail.com>
2022-07-11 19:42:31 +0530

    part2

--- Array/Intermediate.js
+++ Array/Intermediate.js
@@ -50,3 +50,105 @@
 //     }
 //   }
 //   console.log(lostPlanet([1, 5, 7, 8, 3, 0, 2, 6]))
+
+
+
+// Q5)  A manager wants to increment each of its employees' salaries by Rs.1000. 
+//      You are given an array with the current salary of each employee. 
+//      Return the array containing the incremented salary. Use array callback methods.
+
+
+// function goodManager(arr){
+//     let incrementedSalary=[]
+//      for(i=0;i<arr.length;i++){
+//        incrementedSalary [i]=arr[i]+1000
+//      }
+//      return incrementedSalary
+//    }
+//    // console.log(goodManager([1000,2000, 3000]))
+   
+   
+   
+//    function i_salary(callback){
+//      return callback ([1000,2000, 3000]);
+//    }
+//    console.log(i_salary(goodManager))
+
+
+// Q6)   Mickey Mouse gave a task to Goofy. The task is that Mickey will give Goofy an array of numbers and
+//       Goofy will find the square of all the numbers.. Help Goofy to find the square of given numbers. 
+//       Use callback methods.
+
+
+// function square(arr){
+//     let s_arr=[]
+//       for(i=0;i<arr.length;i++){
+//         s_arr [i]=(arr[i]**2)
+//       }
+//       return s_arr
+//     }
+//     // console.log(square([2, 3, 6]))
+//     exports.square = square
+    
+    
+//     function help(callback){
+//       return callback([2, 3, 6])
+//     }
+//     console.log(help(square))
+
+
+
+// Q7)  Amit goes to a restaurant, but after seeing the huge menu. 
+//      He was confused about what to order. Amit wanted to have something which has Paneer and does not have Rice.
+//      Help Amit choose a food item.
+
+
+// function order(menu){
+//     for(let i=0;i<menu.length;i++){
+//       if(menu [i].includes('Paneer') == true && menu [i].includes('Rice') == false){
+//         return (menu [i]);
+//       }
+//     }
+//   }
+//   console.log(order(['Paneer Fried Rice', 'Chicken Biryani','Paneer Paratha', 'Cold Coffee']))
+
+
+// Q8)  Amit was not satisfied with your food choice, 
+//      so he wants you to tell him all the food items that contain Paneer and do not contain Rice.
+
+
+
+// function amitHelp(menu){
+//     let menu_1=[]
+//       for(let i=0;i<menu.length;i++){
+//       if(menu [i].includes('Paneer') == true && menu [i].includes('Rice') == false){
+//            menu_1.push(menu [i])
+//       }
+//     }
+//     return menu_1
+//   }
+//   console.log(amitHelp(['Paneer Butter Masala', 'Paneer Fried Rice', 'Chicken Biryani', 
+//   'Cold Coffee', 'Paneer Kofta']))
+        
+
+
+// Q9)   Given an array of numbers, create a new array by multiplying each number with 
+        //  its square root from the given array.Return original array concatenated with the new array. 
+        //  Use map callback method.
+
+
+        //  function multiply(arr){
+        //      let m_arr=[];
+        //       for(let i=0;i<arr.length;i++){
+        //         m_arr[i]=Math.sqrt(arr[i])*arr[i]
+        //          }
+        //       return m_arr
+        //     }
+        //     // console.log(multiply([4, 9, 25, 49]))
+        //     exports.multiply = multiply
+            
+            
+        //     function multiplyarr(callbacks){
+        //       return callbacks([4, 9, 25, 49]);
+        //     }
+        //     console.log(multiplyarr(multiply))
