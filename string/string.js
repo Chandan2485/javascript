ea42147378a35a214457d2b47bcc504ee7351fc3
Chandan2485 <chandan96kar@gmail.com>
2022-07-20 19:34:25 +0530

    string

---
+++ string/string.js
@@ -0,0 +1,61 @@
+// Q1) Given a sentence and a word, add the given word to the end of the string.
+
+// function appendEnd(str1, str2){
+//     let str11=str1.split('')
+//     let str22=str2.split('')
+//     return str11.concat(str22).join('')
+//   }
+//   console.log(appendEnd('air','campus'))
+
+
+// Q2)  Ravi wrote two javascript programs, each programs have a and b number of bugs. 
+//      Calculate total number of bugs and return "Total number of bugs are a + b". Use template literal
+
+// function numberOfBugs(a, b){
+
+//     let c=a+b
+//     return `Total number of bugs are ${c}`
+   
+//   }
+
+
+// Q3)  Given a string and two index values, return the string from the start index to end index.
+
+// function substring(str, a, b){
+//     return str.slice(a,b)
+//   }
+//   console.log(substring('aircampus',3 ,9))
+
+
+// Q4) Given a string, remove all the whitespaces and then return the new string.
+
+// function removeSpace(str){
+//     let str1=str.split('')
+    
+//    let  str2=str1.filter((s)=>{
+//        return s!=' '
+//      })
+   
+//      return str2.join('')
+//    }
+//    console.log(removeSpace('     java script'))
+
+
+// Q5) Given a string, remove all the whitespaces from the start and then return the new string
+
+// function removeSpace(str){
+//     return str.trimStart()
+//   }
+//   console.log(removeSpace(" air campus "))
+
+
+// Q6) Given a string, remove all the whitespaces from the end and then return the new string
+
+
+// function removeSpace(str){
+//     return str.trimEnd()
+//   }
+//   console.log(removeSpace(" air campus "))
+
+
+  
